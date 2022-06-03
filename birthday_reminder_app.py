19133282e72a7f96c2d278137fea1b1ac2cc817b
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2022-06-03 11:16:42 +0300

    Data validation is working, need to make it nice

--- birthday_reminder_app.py
+++ birthday_reminder_app.py
@@ -23,17 +23,50 @@ Interface CLI
 """
 
 # import pandas as pd
-# df  = pd.read_json('employees.json')
+# df  = pd.read_json('persons.json')
 # #
 # # print(df)
 # print(df.eq('').values.any()) #Suranda ar trukta kurios nors eilutes, arba null reikai patikrinti
 import json
+from datetime import datetime
 
 def validate_birthdate(birthdate):
-    pass
+    # reikia dar patikrinti jeigu gimtadienis 02.29 kad pranestu 02.28
+    # reikia kad data butu praeityje
+
+    if len(birthdate) == 10:
+        try:
+            birthday_date = datetime.strptime(birthdate, '%Y-%m-%d')
+            if birthday_date > datetime.now():
+                #  patikrina ar gimtadienis yra praeityje
+                print('Person is not born yet')
+                raise ValueError
+
+        except ValueError:
+            print('Incorrect date format or date is out of range, it should be YYYY-MM-DD or MM-DD')
+            print(birthdate)
+        else:
+            print('viskas ok')
+    elif len(birthdate) == 5:
+        if birthdate.endswith('02-29'):
+            # If birthday is 02-29 it will act like birthday is on 02-28
+            print('abrakadabra')
+            birthdate = '02-28' # kad ansciau pranestu
+        try:
+            birthday_date = datetime.strptime(birthdate, '%m-%d')
+        except ValueError:
+            print('Incorrect date format or date is out of range, it should be YYYY-MM-DD or MM-DD')
+        else:
+            print('cia irgi ok')
+
+    else:
+        print('Incorrect date format or date is out of range, it should be YYYY-MM-DD or MM-DD')
+
 
 def validate_email(email):
-    pass
+    if '@' not in email or '.' not in email:
+        print('email is not correct')
+
 
 def validate_person_data(data):
     for person in data:
@@ -42,9 +75,10 @@ def validate_person_data(data):
             #  Checks if there is no empty items
             validate_birthdate(person['birthdate'])
             validate_email(person['email'])
-            print('all ok')
+            print('----------------')
         else:
-            print(f'bad news, there is a person with empty : {person}')
+            print(f'bad news, there is a person with empty data: {person}')
+            print('-----------------')
 
 
 
@@ -56,6 +90,8 @@ def parse_data(json_file_name):# check if parsing data is ok, add some logic, mo
             print(data)
     except ValueError:
         print('Data file should be in JSON format!!!')
+    except FileNotFoundError:
+        print('File not found!')
     else:
         validate_person_data(data)
     finally:
@@ -65,5 +101,5 @@ def parse_data(json_file_name):# check if parsing data is ok, add some logic, mo
 
 
 if __name__ == '__main__':
-    parse_data('employees.json')
+    parse_data('persons.json')
 
