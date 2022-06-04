b50c14fe5bd16e606df8f06c3f2f8c50c6a14fd2
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2022-06-04 11:39:40 +0300

    Emails sending is ok(testing is ok), next need to add email text

--- birthday_reminder_app.py
+++ birthday_reminder_app.py
@@ -28,78 +28,91 @@ Interface CLI
 # # print(df)
 # print(df.eq('').values.any()) #Suranda ar trukta kurios nors eilutes, arba null reikai patikrinti
 import json
-from datetime import datetime
-
-def validate_birthdate(birthdate):
-    # reikia dar patikrinti jeigu gimtadienis 02.29 kad pranestu 02.28
-    # reikia kad data butu praeityje
-
-    if len(birthdate) == 10:
-        try:
-            birthday_date = datetime.strptime(birthdate, '%Y-%m-%d')
-            if birthday_date > datetime.now():
-                #  patikrina ar gimtadienis yra praeityje
-                print('Person is not born yet')
-                raise ValueError
-
-        except ValueError:
-            print('Incorrect date format or date is out of range, it should be YYYY-MM-DD or MM-DD')
-            print(birthdate)
-        else:
-            print('viskas ok')
-    elif len(birthdate) == 5:
-        if birthdate.endswith('02-29'):
-            # If birthday is 02-29 it will act like birthday is on 02-28
-            print('abrakadabra')
-            birthdate = '02-28' # kad ansciau pranestu
-        try:
-            birthday_date = datetime.strptime(birthdate, '%m-%d')
-        except ValueError:
-            print('Incorrect date format or date is out of range, it should be YYYY-MM-DD or MM-DD')
-        else:
-            print('cia irgi ok')
-
-    else:
-        print('Incorrect date format or date is out of range, it should be YYYY-MM-DD or MM-DD')
+from datetime import datetime, timedelta
 
 
-def validate_email(email):
-    if '@' not in email or '.' not in email:
-        print('email is not correct')
-
-
-def validate_person_data(data):
-    for person in data:
-        # print(person)
-        if bool(person['name'].strip()) and bool(person['email'].strip() and bool(person['birthdate'].strip())):
-            #  Checks if there is no empty items
-            validate_birthdate(person['birthdate'])
-            validate_email(person['email'])
-            print('----------------')
-        else:
-            print(f'bad news, there is a person with empty data: {person}')
-            print('-----------------')
-
-
-
-
-def parse_data(json_file_name):# check if parsing data is ok, add some logic, more exceptions
+def parse_data(json_file_name):
+    """main function"""
+    # check if parsing data is ok, add some logic,
+    # Checks if we can succesfuly load data from file, if yes, then we send it to validation
     try:
         with open(json_file_name) as json_file:
             data = json.load(json_file)
             print(data)
+            print(type(data))
     except ValueError:
         print('Data file should be in JSON format!!!')
     except FileNotFoundError:
         print('File not found!')
     else:
-        validate_person_data(data)
+        # sending parsed data for validation
+        validate_person_data(data) # if data starts with person(table name) then we need to data['person']
     finally:
         pass
+
+
+def validate_person_data(data):
+    """This f-n validates for empty elements in data"""
+    for person in data:
+        # print(person)
+        if bool(person['name'].strip()) and bool(person['email'].strip() and bool(person['birthdate'].strip())):
+            #  Checks if there is no empty items
+            # If all rows are not empty, then we send tada to next step. Validate birthdate and validate email
+            validate_birthdate(person)
+            validate_email(person)
+            print('----------------')
+        else:
+            print(f'bad news, there is a person with empty data: {person}')
+            print('-----------------')
+    print(data)
+
+
+def validate_email(person):
+    """Simple email validator, check's if @ or . is in string"""
+    if '@' not in person['email'] or '.' not in person['email']:
+        print(f'Person {person["name"]} email is not correct, please check it!')
+
+
+def validate_birthdate(person, birthday_check=False):
+    # reikia dar patikrinti jeigu gimtadienis 02.29 kad pranestu 02.28
+    # reikia kad data butu praeityje
+
+    error_message = f"Person's {person['name']} birthdate {person['birthdate']} format is incorrect " \
+                    f"or date is out of range, it should be YYYY-MM-DD or MM-DD"
+
+    if len(person['birthdate']) == 10:
+        try:
+            birthday_date = datetime.strptime(person['birthdate'], '%Y-%m-%d')
+            if birthday_date > datetime.now():
+                #  patikrina ar gimtadienis yra praeityje
+                print(f'Person {person["name"]} is not born yet')
+                raise ValueError #?? do we need this? if not, then it will execut else
+
+        except ValueError:
+            print(error_message)
+        else:
+            print('viskas ok') #  jeigu viskas ok, galime grazinti duomenys
+            # person['birthday_date'] = birthday_date
+            # print(person)
+            # print(type(person['birthday_date']))
+
+    elif len(person['birthdate']) == 5:
+        if person['birthdate'].endswith('02-29'):
+            # If birthday is 02-29 it will act like birthday is on 02-28, otherwise it throws error
+            print('abrakadabra')
+            person['birthdate'] = '02-28'  # kad ansciau pranestu
+        try:
+            birthday_date = datetime.strptime(person['birthdate'], '%m-%d')
+        except ValueError:
+            print(error_message)
+        else:
+            print('cia irgi ok')
+
+    else:
+        print(error_message)
 
 
 
 
 if __name__ == '__main__':
     parse_data('persons.json')
-
--- email_sender.py
+++ email_sender.py
@@ -1,29 +1,87 @@
-import smtplib #biblioteka susikalbeti su pasto serveriu
+import smtplib  # biblioteka susikalbeti su pasto serveriu
 import ssl
 from email.message import EmailMessage
 from email_settings import sender_email, sender_password, sender_host
 from string import Template
+import time
+
+
+# def send_emails(data, birthday_list):
+#     print(data)
+#     print(birthday_list)
+#     for celebrant in birthday_list:
+#
+#         print(f'I am celebrant {celebrant}')
+#         for person in data:
+#             if person is not celebrant:
+#                 print(f'I am person {person}')
+#             # send email (person, birthday lisit)
+
+
+"""
+Dabara reikies prideti kintamuosius ir sudeti viska i funkcija
+
+"""
 
 
 # reikia pasiimti reikiamus duomenys is kito failo
+def send_emails(data, birthday_list):
+    context = ssl.create_default_context()  # Create a secure SSL context
 
-with open('email_message.html', 'r') as email_message:
-    html_message = email_message.read()
+    with smtplib.SMTP(host=sender_host, port=587) as server:
+        server.ehlo()  # žiūrėkite, kaip į pasisveikinimą su serveriu
+        server.set_debuglevel(1)  # If st to 1 it will display all messages and errors
+        server.starttls(context=context)  # Creating secure chanel
+        server.login(sender_email, sender_password)
 
-html_template = Template(html_message)
+        with open('email_message.html', 'r') as email_message:
+            html_message = email_message.read()
+        html_template = Template(html_message)
+        for celebrant in birthday_list:
+            for person in data:
+                if person is not celebrant:
+                    email = EmailMessage()
+                    email['from'] = sender_email
+                    email['to'] = person['email']  # pakeisti i receiveremail
+                    email['subject'] = f'Birthday reminder'
+                    # Pakeisti i subject, reikia kad prisidetu vardas dar celebratora
+                    data_for_substitution = {'name': person['name'],
+                                             'celebrant_name': celebrant['name'],
+                                             'birthdate': person['birthdate'][-5:],
+                                             'days_left': '7'} # reikia gal days_left padaryti kintamaji
 
-email = EmailMessage()
-email['from'] = sender_email
-email['to'] = 'polujarek@gmail.com' # pakeisti i receiveremail
-email['subject'] = 'Paskutinis testas16,41' # Pakeisti i subject
-email.set_content(html_template.substitute({'name': 'Jaroslav'}), 'html') #  Pakeicia reiksmes . Galima arba zodyna, arba per lygu
+                    email.set_content(html_template.substitute(data_for_substitution), 'html')
+                    server.send_message(email)
+                    print(email)
+                    # cia gal reikia prideti laukima?
+                    time.sleep(5)
 
-context = ssl.create_default_context()  # Create a secure SSL context
 
-with smtplib.SMTP(host=sender_host, port=587) as server:
-    server.ehlo()# žiūrėkite, kaip į pasisveikinimą su serveriu
-    server.set_debuglevel(1) #  If st to 1 it will display all messages and errors
-    server.starttls(context=context)# Creating secure chanel
-    server.login(sender_email, sender_password)
 
-    server.send_message(email)
+
+""" reikia pratestuoti ar veikia tokia sistema su savo emailais
+ padaryti emailo templata
+ padaryti subject
+ padaryti emaile kad rodytu normalia data vietoje skaiciu pvz march 30(bet jei bus laiko) nebutimnai
+ """
+
+# with open('email_message.html', 'r') as email_message:
+#     html_message = email_message.read()
+# html_template = Template(html_message)
+#
+# email = EmailMessage()
+# email['from'] = sender_email
+# email['to'] = 'polujarek@gmail.com'  # pakeisti i receiveremail
+# email['subject'] = 'Paskutinis testas16,41'  # Pakeisti i subject
+# email.set_content(html_template.substitute({'name': 'Jaroslav'}),
+#                   'html')  # Pakeicia reiksmes . Galima arba zodyna, arba per lygu
+#
+# context = ssl.create_default_context()  # Create a secure SSL context
+#
+# with smtplib.SMTP(host=sender_host, port=587) as server:
+#     server.ehlo()  # žiūrėkite, kaip į pasisveikinimą su serveriu
+#     server.set_debuglevel(1)  # If st to 1 it will display all messages and errors
+#     server.starttls(context=context)  # Creating secure chanel
+#     server.login(sender_email, sender_password)
+#
+#     server.send_message(email)
