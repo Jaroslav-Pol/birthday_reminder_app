88e88acf8caeddca565ca5e0979a4f6bd8f41af2
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2022-06-06 17:15:42 +0300

    App is starting, command 1, 9, is working, handling errors - ok, lgtm
    next need to make command 2

--- birthday_reminder_app.py
+++ birthday_reminder_app.py
@@ -22,97 +22,139 @@ Exit once emails are sent.
 Interface CLI
 """
 
-# import pandas as pd
-# df  = pd.read_json('persons.json')
-# #
-# # print(df)
-# print(df.eq('').values.any()) #Suranda ar trukta kurios nors eilutes, arba null reikai patikrinti
 import json
 from datetime import datetime, timedelta
+import time
+
+error_list = []
+
+"""
+Reikes gal prideti count errors in data
+Reikia padaryti apsauga nuo durniaus, kad negalima butu issiusti emailu, jeigu count_errors nelygus 0
+reikia kad atspausdintu kad viskas yra gerai, jeigu viskas yra gerai, galima ji prideti i finaly 
+"""
 
 
 def parse_data(json_file_name):
-    """main function"""
-    # check if parsing data is ok, add some logic,
-    # Checks if we can succesfuly load data from file, if yes, then we send it to validation
+    """Checking if data parsing is ok, if yes, sending data for validation
+    or we need to return it better?"""
     try:
         with open(json_file_name) as json_file:
             data = json.load(json_file)
-            print(data)
-            print(type(data))
     except ValueError:
-        print('Data file should be in JSON format!!!')
-    except FileNotFoundError:
-        print('File not found!')
+        print('Data file should be in JSON format!!!\n----------')
+    except FileNotFoundError and OSError:
+        print('File not found! Please enter correct path and filename.\n-----------')
     else:
-        # sending parsed data for validation
-        validate_person_data(data) # if data starts with person(table name) then we need to data['person']
+        # Sending parsed data for validation
+        print('Data parsing is ok.\n----------')
+        time.sleep(2)
+        if type(data) == list:
+            # Checking is data from json file with table name or without
+            validate_person_data(data)
+        elif type(data) == dict:
+            validate_person_data(data['persons'])
+
     finally:
         pass
 
 
 def validate_person_data(data):
-    """This f-n validates for empty elements in data"""
+    """Validates for empty elements in data, if it's ok, then checking date and email"""
+    error_list.clear()
     for person in data:
-        # print(person)
         if bool(person['name'].strip()) and bool(person['email'].strip() and bool(person['birthdate'].strip())):
-            #  Checks if there is no empty items
-            # If all rows are not empty, then we send tada to next step. Validate birthdate and validate email
+            # Checks if there is no empty items
             validate_birthdate(person)
+            # Checks birthday date
             validate_email(person)
-            print('----------------')
+            # Checks email
         else:
-            print(f'bad news, there is a person with empty data: {person}')
-            print('-----------------')
-    print(data)
+            print(f'There is a person with empty data: {person}\n----------')
+            error_list.append(person['name'])
+    if len(error_list) > 0:
+        print(f'Found {len(error_list)} errors with data, please check!')  # Maybe add names from error list?
+    else:
+        print('Data is OK!')
 
 
 def validate_email(person):
     """Simple email validator, check's if @ or . is in string"""
     if '@' not in person['email'] or '.' not in person['email']:
-        print(f'Person {person["name"]} email is not correct, please check it!')
+        print(f'Person {person["name"]} email is not correct, please check it!\n----------')
+        error_list.append(person['name'])
 
 
-def validate_birthdate(person, birthday_check=False):
-    # reikia dar patikrinti jeigu gimtadienis 02.29 kad pranestu 02.28
-    # reikia kad data butu praeityje
-
+def validate_birthdate(person):
+    """Validates person birthday date"""
     error_message = f"Person's {person['name']} birthdate {person['birthdate']} format is incorrect " \
-                    f"or date is out of range, it should be YYYY-MM-DD or MM-DD"
+                    f"or date is out of range, it should be YYYY-MM-DD or MM-DD!\n----------"
 
     if len(person['birthdate']) == 10:
+        # If date format is YYYY-MM-DD
         try:
             birthday_date = datetime.strptime(person['birthdate'], '%Y-%m-%d')
             if birthday_date > datetime.now():
-                #  patikrina ar gimtadienis yra praeityje
-                print(f'Person {person["name"]} is not born yet')
-                raise ValueError #?? do we need this? if not, then it will execut else
+                #  Checks if date is in past
+                print(f'Person {person["name"]} is not born yet!\n----------')
+                error_list.append(person['name'])
 
         except ValueError:
             print(error_message)
-        else:
-            print('viskas ok') #  jeigu viskas ok, galime grazinti duomenys
-            # person['birthday_date'] = birthday_date
-            # print(person)
-            # print(type(person['birthday_date']))
+            error_list.append(person['name'])
 
     elif len(person['birthdate']) == 5:
-        if person['birthdate'].endswith('02-29'):
-            # If birthday is 02-29 it will act like birthday is on 02-28, otherwise it throws error
-            print('abrakadabra')
-            person['birthdate'] = '02-28'  # kad ansciau pranestu
-        try:
-            birthday_date = datetime.strptime(person['birthdate'], '%m-%d')
-        except ValueError:
-            print(error_message)
+        #  If date format is MM-DD
+        if person['birthdate'] == '02-29':
+            pass
         else:
-            print('cia irgi ok')
+            try:
+                birthday_date = datetime.strptime(person['birthdate'], '%m-%d')  ## gal cia reikia pasalinti kintamaji?
+            except ValueError:
+                print(error_message)
+                error_list.append(person['name'])
 
     else:
         print(error_message)
+        error_list.append('+')
 
 
+def start_app():
+    print('Hello! \nPlease choose what you want to do: ')
+    time.sleep(1)  # For testing, maybe need to leave it?
+    while True:
+        print('----------')
+        command = input('1. Validate persons data file.\n'
+                        '2. Check for birthdays and send reminder emails.\n'
+                        '9. Exit\n')
+        time.sleep(1)
+        if command == '1':
+            print('Program will validate persons data file. File should be in JSON format.')
+            path_to_file = input('Please enter path to the file: \n')
+            file_name = input('Please enter file name: \n')
+            # time.sleep(1)
+            if not path_to_file:
+                #  If path is a program folder
+                parse_data(file_name)
+            else:
+                parse_data(path_to_file + '\\' + file_name)
+            continue
+
+        elif command == '2':
+            print('')  # Send emails
+            time.sleep(5)
+            print('emails successfully sent')
+            break
+
+        elif command == '9':
+            print('Program successfully closed!')
+            time.sleep(2)
+            break
+
+        else:
+            print('Incorrect input, please try again!')
+            continue
 
 
 if __name__ == '__main__':
-    parse_data('persons.json')
+    start_app()
