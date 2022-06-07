2d8d6cf0227fb109cd28e0ceaca24d9cd3d1ca93
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2022-06-07 20:17:00 +0300

    App is working, 2 command - ok, added birthday check, email sending
    Maybe need add some logic after birthday check?

--- birthday_reminder_app.py
+++ birthday_reminder_app.py
@@ -1,8 +1,7 @@
 """
 Cron (run automaticaly every day)
-Send emails to everyone except person who celebrates
-One week before birthday
-1. Valdate persons birthday data file, handle errors
+Send emails to everyone except person who celebrates one week before birthday
+1. Validate persons birthday data file, handle errors
      A person's entry in the file will contain:
         1.the person's name,
         2.email,
@@ -22,66 +21,68 @@ Exit once emails are sent.
 Interface CLI
 """
 
-
 import time
 from data_validator import parse_data
-
-"""
-Reikes gal prideti count errors in data
-Reikia padaryti apsauga nuo durniaus, kad negalima butu issiusti emailu, jeigu count_errors nelygus 0
-reikia kad atspausdintu kad viskas yra gerai, jeigu viskas yra gerai, galima ji prideti i finaly 
-"""
-
-
-
+from birthdays_checker import check_for_birthday
+from email_sender import send_emails
 
 
 def start_app():
-    print('Hello! \nPlease choose what you want to do: ')
-    time.sleep(1)  # For testing, maybe need to leave it?
+    print("Hello!\nIt's birthday reminder app \nPlease choose what you want to do: ")
+    time.sleep(1)
     while True:
         print('----------')
         command = input('1. Validate persons data file.\n'
-                        '2. Check for birthdays and send reminder emails.\n'
+                        '2. Check birthdays, send reminder emails.\n'
                         '9. Exit\n')
         time.sleep(1)
-        # gal cia reikia kintamuju prideti
-        if command == '1':
-            print('Program will validate persons data file. File should be in JSON format.')
-
-            #reikia gal cia funkcija padaryti?
-            path_to_file = input('Please enter path and file name: \n')
-            #  Reikia gal prideti gale .json???
-            data = parse_data(path_to_file)
-            continue
-
-        elif command == '2':
-            if 'data' in locals() and data is not None: #  or we need to use data? patikriname ar data jau yra kaip kintamasis ir jeigu yra, ar nera tuscias
-                #  reikia gal dar patikrinti ar buvo errorai, jeigu taip, reikia is pradziu aptikrinti?
-
-                print(data)
-                print('labas vakaras')
-
-            else:
-                print(data)
-                print('Duomenys nepatikrinti, rekomenduojame is pradziu patikrinti duomenys')
+        match command:
+            case '1':  # Validate persons data file, returns data if it's valid
+                print(
+                    'Program will validate persons data file. File should be in JSON format.')  # Or add .json in the end?
                 path_to_file = input('Please enter path and file name: \n')
-                data = parse_data(path_to_file, birthday_check=True)
-                print(data)
-            print('')  # Send emails
-            time.sleep(3)
-            print('emails successfully sent')
-            break
+                data = parse_data(path_to_file)  # Returns data if it's valid, or None
+                continue
 
-        elif command == '9':
-            print('Program successfully closed!')
-            time.sleep(2)
-            break
+            case '2':  # Checks for birthdays, send emails
+                if 'data' in locals() and data is not None:
+                    #  Checks if case 1 was executed before, and or data were valid
+                    birthday_list = check_for_birthday(data)
+                else:
+                    print('Data is not verified, we strongly recommend validate data first')
+                    path_to_file = input('Please enter path and file name, or enter 9 to return\n')
+                    if path_to_file == '9':
+                        continue
+                    else:
+                        data = parse_data(path_to_file, birthday_check=True)
+                        # If birthday_check, then data would be only parsed, not validated
 
-        else:
-            print('Incorrect input, please try again!')
-            continue
+                    if data is not None:
+                        birthday_list = check_for_birthday(data)
+                    else:
+                        continue
 
+                if birthday_list:  # If list is not empty
+                    print(f'There are {len(birthday_list)} birthdays after 7 days.')
+                    print('Sending emails')
+                    time.sleep(2)
+                    # input('Send reminder emails y/n?')  # ar nereikia klausti???????????????????
+                    send_emails(data, birthday_list)
+                    print('Emails successfully sent, program will close in 5 sec')  # Really successfully???????????//
+                    time.sleep(5)
+                    break
+                else:
+                    print('Program will close in 5 sec.')
+                    time.sleep(5)
+                    break
+            case '9':  # Exit
+                print('Program will close in 5 seconds.')
+                time.sleep(5)
+                break
+
+            case _:  # Incorrect input
+                print('Incorrect input, please try again!')
+                continue
 
 if __name__ == '__main__':
     start_app()
